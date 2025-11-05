35394f73957ab4e505fcd542f6c589682b3c852c
Michael <srej100@gmail.com>
2025-11-05 19:49:20 +0100

    re-enabled randomisation

--- Facilitation-game.py
+++ Facilitation-game.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 
-RANDOMIZE_OPTION_ORDER = False
+RANDOMIZE_OPTION_ORDER = True
 
 class SocialEntrepreneurGame:
     def __init__(self):
